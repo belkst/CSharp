3a8f05d5ca2e8fe666dfe02c7e31b7a9723cb38e
Aleksandr Novikov <belkst@mail.ru>
2022-09-15 23:40:35 +0300

    Составил блок-схему и написал код первого ДЗ HomeWork01 задача №1 - Task01

--- HomeWork01/Task01/Program.cs
+++ HomeWork01/Task01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1: Выдаем максимальные значения из двух чисел
+int a = -9;
+int b = -3;
+int max = 0;
+Console.WriteLine("значение a= " +a);
+Console.WriteLine("значение b= " +b);
+
+if(a<b)
+{
+    max = b;
+}
+
+{
+    max = a;
+}
+
+Console.Write("Maксимальное значение: ");
+Console.WriteLine(max);
