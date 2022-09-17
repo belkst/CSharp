619b6a00f67cc88ec21899fe4c37d12234571539
Konstantin <kvsokolov@live.com>
2022-09-18 02:52:49 +0300

    while2for-in loop, add dot/decimal logic

--- HomeWork01/Task04/Program.cs
+++ HomeWork01/Task04/Program.cs
@@ -1,12 +1,11 @@
 // Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.Write("Read number, please: ");
-int N = Convert.ToInt32(Console.ReadLine());  // Ввод числа
-int i=0;                                       // Инициализация переменной
+int N = Convert.ToInt32(Console.ReadLine());
 
-while (i<=N)
-{
-    if(i % 2 == 0) Console.Write(i + ", ");
-    i ++;
+for (int i = 2; i <= N; i++) {
+    if(i % 2 == 0) {
+        Console.Write(i);
+        if (i < N - 1) Console.Write(", ");
+        if (i >= N - 1) Console.Write(".\n");
+    }
 }
-Console.Write("\b"+"\b"); //удалил пробел и последнюю запятую
-Console.Write("."+ "\n"); //точка и новая строка
