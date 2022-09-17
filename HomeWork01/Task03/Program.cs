9c3b1622f6e08bfae14024d2b60025f6b4026993
Aleksandr Novikov <belkst@mail.ru>
2022-09-18 00:56:11 +0300

    Add code in Task03, add diagramm and code in Task04

--- HomeWork01/Task03/Program.cs
+++ HomeWork01/Task03/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//3адача №3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Read number, please: ");
+int N = Convert.ToInt32(Console.ReadLine());  // Ввод числа
+
+    if(N % 2 == 0) Console.Write( "ДА");
+    else Console.Write("НЕТ");
+
+Console.Write("."+ "\n"); //точка и новая строка
--- HomeWork01/Task04/Program.cs
+++ HomeWork01/Task04/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Read number, please: ");
+int N = Convert.ToInt32(Console.ReadLine());  // Ввод числа
+int i=0;                                       // Инициализация переменной
+
+while (i<=N) // добавил меньше или равно
+{
+    if(i % 2 == 0) Console.Write(i + ", ");
+    i ++;
+}
+Console.Write("\b"+"\b"); // ооооо заебок)
+
+Console.Write("."+ "\n"); //точка и новая строка
+
+
+// теперь все красиво // теперь да) очень красиво)
+
+
+// надо еще к 3й задаче код написать, но это я уже завтра утром буду делать
+// а втретьей задаче то что? так это легче 4ой if esle
