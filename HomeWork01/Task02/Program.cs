9992e5b2ca6cf3a0f0b6335bbad45b31666f437b
Aleksandr Novikov <belkst@mail.ru>
2022-09-17 13:06:32 +0300

    Оптимизировал код

--- HomeWork01/Task02/Program.cs
+++ HomeWork01/Task02/Program.cs
@@ -4,14 +4,8 @@ int b = 5;
 int c = 78;
 int max = a;
 
-if(max<b)
-{
-    max = b;
-}
-    if(max<c)
-    {
-        max = c;
-    }
+if(max<b) max = b;
+if(max<c) max = c;
 
 Console.WriteLine("значение a= " +a);
 Console.WriteLine("значение b= " +b);
