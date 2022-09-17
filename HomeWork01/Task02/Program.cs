c57a88a1114917a3a4b7a2b63095def04e80c110
Aleksandr Novikov <belkst@mail.ru>
2022-09-17 12:39:14 +0300

    Добавил диаграмму на вторую задачу и написал код. Время работы 30 минут

---
+++ HomeWork01/Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 44;
+int b = 5;
+int c = 78;
+int max = a;
+
+if(max<b)
+{
+    max = b;
+}
+    if(max<c)
+    {
+        max = c;
+    }
+
+Console.WriteLine("значение a= " +a);
+Console.WriteLine("значение b= " +b);
+Console.WriteLine("значение c= " +c);
+Console.Write("Из них максимальное значение = ");
+Console.WriteLine(max);
