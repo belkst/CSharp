659715bfed819498297c0fc3008946494a7d2a7b
Aleksandr Novikov <belkst@mail.ru>
2022-09-20 19:42:03 +0300

    Co-authored-by: lint83@list.ru <lint83@list.ru>

---
+++ Lecture02/Examples/Example009_IntroArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//Нахождение максимального значения из 9
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {14, 28, 683, 4, 25, 63, 78, 78, 59};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+ );
+
+Console.WriteLine(max);
---
+++ Lecture02/Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 23, 34, 54, 65, 47, 4, 68, 78, 4, 98, 8 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
