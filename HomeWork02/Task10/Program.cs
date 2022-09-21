b4ee9398d7d80d40eacad52341a63ee51f8fd9e1
Fly <lint83@list.ru>
2022-09-21 23:34:38 +0300

    Revert "камент"
    This reverts commit 74fae78ae914e1516bcc1cc101e203239c4c64d8.

--- HomeWork02/Task10/Program.cs
+++ HomeWork02/Task10/Program.cs
@@ -1,4 +1,4 @@
 ﻿int num = -915;
 Console.WriteLine("В этой задаче мы покажем вторую цифру числа: " +num);
 num = num/10;
-Console.WriteLine("вторая цыфра этого числа: " +Math.Abs(num%10)); // нужно брать по модулю
+Console.WriteLine("вторая цыфра этого числа: " +Math.Abs(num%10));
