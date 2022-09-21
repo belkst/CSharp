222281b6966286ade007e07260471dac4cf3ee43
Aleksandr Novikov <belkst@mail.ru>
2022-09-21 23:58:49 +0300

    Добавил код к 15 задаче 2

--- HomeWork02/Task15/Program.cs
+++ HomeWork02/Task15/Program.cs
@@ -1,8 +1,9 @@
 ﻿int numDay = -4;
 
-if(numDay == 6 || numDay == 7)  Console.WriteLine("Выбранный день - "+numDay+" является выходным днем");
-else {
-    if (numDay >0 & numDay<6 ) Console.WriteLine ("Этот день - "+numDay+" является будним!");
-    else Console.WriteLine ("Это число - "+numDay+" не является числом, указывающим на день недели!");
-    }
+if (numDay == 6 || numDay == 7) Console.WriteLine("Выбранный день - " + numDay + " является выходным днем");
+else
+{
+    if (numDay > 0 & numDay < 6) Console.WriteLine("Этот день - " + numDay + " является будним!");
+    else Console.WriteLine("Это число - " + numDay + " не является числом, указывающим на день недели!");
+}
 
