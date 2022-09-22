caaf7818b0b2637f71240879fcb59e2597db1b4e
Aleksandr Novikov <belkst@mail.ru>
2022-09-22 19:48:43 +0300

    Добавил блок схему и код к задаче 228

---
+++ HomeWork02/Task228/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Cредне-арифметическое заданного массива ровняется - ");
+int[] numbers = { 10, 20, 30, 40, 50, 60, 70 };
+int result = 0;
+for (int i = 0; i < numbers.Length; i++) result = result + numbers[i];
+
+Console.WriteLine(result / numbers.Length);
