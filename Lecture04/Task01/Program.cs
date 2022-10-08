12fa0c4e94d1df97e00b5254ba5bf0fc2c26c428
Aleksandr Novikov <belkst@mail.ru>
2022-10-08 19:43:14 +0300

    Добавил код с лекции

---
+++ Lecture04/Task01/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+void printArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+
+
+        }
+        //    Console.ReadLine();
+        Console.WriteLine();
+    }
+}
+
+void fillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"Введите поочереди значение ячейки {i},{j} = ");
+            matr[i, j] = Convert.ToInt32(Console.ReadLine()); //new Random().Next(1, 10);
+        }
+
+    }
+}
+
+int[,] matrix = new int[3, 4];
+
+
+printArray(matrix);
+fillArray(matrix);
+printArray(matrix);
---
+++ Lecture04/Task02_Factorials/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n* Factorial(n-1);
+// }
+
+// for (int i = 0; i < 40; i++)
+// {
+//     Console.WriteLine(Factorial(i));
+// }
+
+
+int Fibonacchi(int n)
+{
+    if(n==1||n==2) return 1;
+    else return Fibonacchi(n-1)+Fibonacchi(n-2);
+}
+
+for (int i=1; i<50; i++)
+{
+    System.Console.WriteLine($"{i} = {Fibonacchi(i)}");
+}
