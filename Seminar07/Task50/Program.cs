7e778afcfcef085907971f2a97936980dfb3652c
Aleksandr Novikov <belkst@mail.ru>
2022-10-09 22:12:18 +0300

    Добавитл решение задачи 50

---
+++ Seminar07/Task50/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 50.
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет
+
+Console.Clear();
+Console.WriteLine("Эта программа возвращает значение элемента массива, указанного пользователем.");
+System.Console.WriteLine("--------------------------------");
+Console.Write("Введите номер строки: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+int[,] Array = new int[5, 5];
+for (int i = 0; i < Array.GetLength(0); i++)
+{
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        Array[i, j] = new Random().Next(0, 100);
+        Console.Write($" {Array[i, j]}");
+    }
+    Console.WriteLine("\t");
+}
+Console.WriteLine();
+
+if (m >= Array.GetLength(0) || n >= Array.GetLength(1))
+{
+    Console.WriteLine($"Введенные значения строки {m} и столбца {n} выходят за пределы массива\n");
+}
+else
+{
+    int i = m;
+    int j = n;
+    Console.WriteLine($"В указанной Вами ячейке ({i},{j}) массива хранится значение: {Array[i, j]}\n");
+}
