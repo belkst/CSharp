01e5608057bebe1c2d6bb4aca514f84ee55df689
Aleksandr Novikov <belkst@mail.ru>
2022-10-11 20:26:21 +0300

    Решил задачу 52

--- Seminar07/Task52/Program.cs
+++ Seminar07/Task52/Program.cs
@@ -7,7 +7,7 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 Console.Clear();
-
+System.Console.WriteLine("Задан следующий массив:\n");
 int[,] Array = new int[5, 5];
 for (int i = 0; i < Array.GetLength(0); i++)
 {
@@ -19,17 +19,20 @@ for (int i = 0; i < Array.GetLength(0); i++)
     Console.WriteLine("\t");
 }
 Console.WriteLine();
-
+System.Console.WriteLine("Среднее арифметическое столбцов равно:");
 double[] ArrResult = new double[Array.GetLength(1)];
 double Result = 0;
 for (int j = 0; j < Array.GetLength(0); j++)
 {
     for (int i = 0; i < Array.GetLength(1); i++)
     {
-        Result = Result + Array[i,j]/Array.GetLength(1);
-        System.Console.Write(Result+" ");
+        Result = Result + Array[i,j];
+       //System.Console.Write(Result+" ");
     }
 
-    ArrResult[j] = Result; 
-    Console.Write($"{ArrResult[j]} - ");
+    ArrResult[j] = Result/Array.GetLength(1); 
+    Console.Write($"столбец {j} = {ArrResult[j]}");
+    Result=0;
+    System.Console.WriteLine("");
 }
+System.Console.WriteLine("====================================");
